d3885a7304ef0d4d832b44e105549ef700f8b912
LodolaTomas <tomaslodola1@gmail.com>
2022-06-06 10:43:21 -0300

    create views and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostlistComponent } from './pages/postlist/postlist.component';
+import { PostdetailComponent } from './pages/postdetail/postdetail.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostlistComponent,
+    PostdetailComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
