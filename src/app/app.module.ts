1d234d20f584212b5bd19c422de8d6b23ddf64d0
LodolaTomas <tomaslodola1@gmail.com>
2022-06-06 15:19:32 -0300

    add view header with bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { PostsModule } from './pages/posts/posts.module';
-import { HeaderComponent } from './shared/header/header.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     PageNotFoundComponent,
-    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    PostsModule
+    PostsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
