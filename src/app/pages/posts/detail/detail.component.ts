bb7edeb685599fbfb745834532de6a37b228126d
LodolaTomas <tomaslodola1@gmail.com>
2022-06-06 15:21:29 -0300

    implementing styles to details.component

--- src/app/pages/posts/detail/detail.component.ts
+++ src/app/pages/posts/detail/detail.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ConfirmationService } from 'primeng/api';
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.scss']
+  styleUrls: ['./detail.component.scss'],
 })
 export class DetailComponent implements OnInit {
+  post = {
+    userId: 1,
+    id: 1,
+    title:
+      'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+  };
+  comments = [
+    {
+      email: 'jramonda@mobydigital.com',
+      name: 'Juan Ramonda',
+      comment:
+        'quo vero reiciendis velit similique earum\nquisquam dolores velit et non\naliquid qui nesciunt est\net iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident',
+    },
+    {
+      email: 'afranco@mobydigital.com',
+      name: 'Antonio Franco',
+      comment:
+        'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
+    },
+  ];
+  isTextAreaDisabled: boolean = true;
+  constructor(private confirmationService: ConfirmationService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  deleteComment(email: string) {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete this comment?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.comments = this.comments.filter(
+          (comment) => comment.email != email
+        );
+      },
+    });
   }
-
 }
--- src/app/pages/posts/posts.module.ts
+++ src/app/pages/posts/posts.module.ts
@@ -6,7 +6,14 @@ import { DetailComponent } from './detail/detail.component';
 import { RouterModule } from '@angular/router'; 
 
 import {TableModule} from 'primeng/table';
-
+import {DividerModule} from 'primeng/divider';
+import {PanelModule} from 'primeng/panel';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import {ToolbarModule} from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
 @NgModule({
   declarations: [
     ListComponent,
@@ -15,7 +22,15 @@ import {TableModule} from 'primeng/table';
   imports: [
     CommonModule,
     RouterModule,
-    TableModule
-  ]
+    TableModule,
+    DividerModule,
+    PanelModule,
+    InputTextareaModule,
+    ToolbarModule,
+    ButtonModule,
+    SplitButtonModule,
+    ConfirmDialogModule
+  ],
+  providers: [ConfirmationService]
 })
 export class PostsModule { }
