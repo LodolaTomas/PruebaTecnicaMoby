72d09a60e9207a103f0a4a089ac34053385fc03b
LodolaTomas <tomaslodola1@gmail.com>
2022-06-07 02:03:26 -0300

    pass the comment logic to a component

--- src/app/pages/posts/detail/detail.component.ts
+++ src/app/pages/posts/detail/detail.component.ts
@@ -1,47 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { ConfirmationService } from 'primeng/api';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Post } from 'src/app/models/interface/post.interface';
+import { PostsService } from 'src/app/service/posts.service';
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss'],
 })
-export class DetailComponent implements OnInit {
-  post = {
-    userId: 1,
-    id: 1,
-    title:
-      'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-  };
-  comments = [
-    {
-      email: 'jramonda@mobydigital.com',
-      name: 'Juan Ramonda',
-      comment:
-        'quo vero reiciendis velit similique earum\nquisquam dolores velit et non\naliquid qui nesciunt est\net iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident',
-    },
-    {
-      email: 'afranco@mobydigital.com',
-      name: 'Antonio Franco',
-      comment:
-        'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
-    },
-  ];
-  isTextAreaDisabled: boolean = true;
-  constructor(private confirmationService: ConfirmationService) {}
+export class DetailComponent implements OnInit{
+  post!: Post;
+  dateLastComment!:string;
+  constructor(private postSrv: PostsService, private route: ActivatedRoute) {}
 
-  ngOnInit(): void {}
-
-  deleteComment(email: string) {
-    this.confirmationService.confirm({
-      message: 'Are you sure you want to delete this comment?',
-      header: 'Confirmation',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.comments = this.comments.filter(
-          (comment) => comment.email != email
-        );
-      },
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.postSrv.getPost(params['id']).subscribe((data) => {
+        this.post = data;
+      });
     });
+  }
+
+  getLastComment(date:Date):void{
+    let year = date.getUTCFullYear()
+    let month = date.toLocaleString('en', { month: 'long' })
+    let day = date.getDate();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    this.dateLastComment = `${year}, ${month}, ${day}, ${hour}:${minute}`
   }
 }
