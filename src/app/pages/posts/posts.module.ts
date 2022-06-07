ce15bf1254721352083c0c780593067faf2a6151
LodolaTomas <tomaslodola1@gmail.com>
2022-06-07 05:04:18 -0300

    add newComment and ordering things

--- src/app/pages/posts/posts.module.ts
+++ src/app/pages/posts/posts.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ListComponent } from './list/list.component';
-import { DetailComponent } from './detail/detail.component';
 
 import { RouterModule } from '@angular/router'; 
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
+import { PostsService } from 'src/app/service/posts.service';
+import { CommentsComponent } from './components/comments/comments.component';
+import { NewCommentComponent } from './components/new-comment/new-comment.component';
+
+import {InputTextModule} from 'primeng/inputtext';
 import {TableModule} from 'primeng/table';
 import {DividerModule} from 'primeng/divider';
 import {PanelModule} from 'primeng/panel';
@@ -17,14 +22,13 @@ import { SplitButtonModule } from 'primeng/splitbutton';
 import {ConfirmDialogModule} from 'primeng/confirmdialog';
 import { ConfirmationService } from 'primeng/api';
 import {FieldsetModule} from 'primeng/fieldset';
-import {InputTextModule} from 'primeng/inputtext';
-import { PostsService } from 'src/app/service/posts.service';
-import { CommentsComponent } from './components/comments/comments.component';
+import {ToastModule} from 'primeng/toast';
 @NgModule({
   declarations: [
     ListComponent,
     DetailComponent,
-    CommentsComponent
+    CommentsComponent,
+    NewCommentComponent
   ],
   imports: [
     CommonModule,
@@ -40,7 +44,8 @@ import { CommentsComponent } from './components/comments/comments.component';
     SplitButtonModule,
     ConfirmDialogModule,
     FieldsetModule,
-    InputTextModule
+    InputTextModule,
+    ToastModule
   ],
   providers: [ConfirmationService,PostsService]
 })
