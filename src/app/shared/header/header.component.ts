6400483a6d4668ea9936308841f7de552ab79a29
LodolaTomas <tomaslodola1@gmail.com>
2022-06-07 12:42:42 -0300

    validating which button to show in the navbar

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  constructor() { }
+  islogged:boolean = false;
+  constructor(private router:Router,private authSrv:AuthService) { }
 
   ngOnInit(): void {
+    this.authSrv.isUserLoggedIn.subscribe({
+      next: (isLogged) => {
+        this.islogged = isLogged;
+      }
+    })
+  }
+
+  redirectTo(path:string):void{
+    this.router.navigate([path]);
+  }
+
+  logOut():void{
+    this.authSrv.logOut();
   }
 
 }
