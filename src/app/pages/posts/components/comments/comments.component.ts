d71308eb1fd23008f2f53f11fbfdea20fddc46b9
LodolaTomas <tomaslodola1@gmail.com>
2022-06-07 05:06:21 -0300

    removing hardcode for dynamic comments

--- src/app/pages/posts/components/comments/comments.component.ts
+++ src/app/pages/posts/components/comments/comments.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ConfirmationService } from 'primeng/api';
 import { Comment } from 'src/app/models/interface/comment.interface';
+import { StorageService } from 'src/app/service/storage.service';
 
 @Component({
   selector: 'app-comments',
@@ -9,30 +10,19 @@ import { Comment } from 'src/app/models/interface/comment.interface';
 })
 export class CommentsComponent implements OnInit {
 
-  @Output() lastComment:EventEmitter<Date> = new EventEmitter();
-
-  comments:Comment[] = [
-    {
-      id:1,
-      email: 'jramonda@mobydigital.com',
-      name: 'Juan Ramonda',
-      comment:
-        'quo vero reiciendis velit similique earum\nquisquam dolores velit et non\naliquid qui nesciunt est\net iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident',
-      date: new Date(),
-    },
-    {
-      id:2,
-      email: 'afranco@mobydigital.com',
-      name: 'Antonio Franco',
-      comment:
-        'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
-      date: new Date(),
-    },
-  ];
+  @Output() lastComment:EventEmitter<string> = new EventEmitter();
+  @Input() postId!:number;
+  comments!:Comment[];
   isTextAreaDisabled: boolean = true;
-  constructor(private confirmationService: ConfirmationService,) { }
+  constructor(private confirmationService: ConfirmationService,private storageSrv:StorageService) { }
 
   ngOnInit(): void {
+    this.storageSrv.subscriptionPostComments.subscribe({
+      next: (comentInPosts) => {
+        this.comments = comentInPosts.map(comentInPost => comentInPost.postId === this.postId ? comentInPost.comments : []).flat();
+      }
+    })
+    this.comments = this.storageSrv.getPostCommentsByPostId(this.postId)?.comments ? this.storageSrv.getPostCommentsByPostId(this.postId)?.comments! : [];
   }
 
   deleteComment(id:number):void {
